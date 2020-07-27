f70e940e39d9c0600747df79f4dfc4f6f9e07fc1
Jongmin Kim <whdalsrnt@megazone.com>
2020-07-27 13:59:48 +0900

    Change spaceone run command
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/spaceone/identity/conf/proto_conf.py
+++ src/spaceone/identity/conf/proto_conf.py
@@ -1,16 +1,14 @@
 PROTO = {
-    'v1': {
-        'domain': ['Domain'],
-        'domain_owner': ['DomainOwner'],
-        'provider': ['Provider'],
-        'service_account': ['ServiceAccount'],
-        'project_group': ['ProjectGroup'],
-        'project': ['Project'],
-        'policy': ['Policy'],
-        'role': ['Role'],
-        'user': ['User'],
-        'api_key': ['APIKey'],
-        'token': ['Token'],
-        'authorization': ['Authorization'],
-    }
+    'spaceone.identity.api.v1.domain': ['Domain'],
+    'spaceone.identity.api.v1.domain_owner': ['DomainOwner'],
+    'spaceone.identity.api.v1.provider': ['Provider'],
+    'spaceone.identity.api.v1.service_account': ['ServiceAccount'],
+    'spaceone.identity.api.v1.project_group': ['ProjectGroup'],
+    'spaceone.identity.api.v1.project': ['Project'],
+    'spaceone.identity.api.v1.policy': ['Policy'],
+    'spaceone.identity.api.v1.role': ['Role'],
+    'spaceone.identity.api.v1.user': ['User'],
+    'spaceone.identity.api.v1.api_key': ['APIKey'],
+    'spaceone.identity.api.v1.token': ['Token'],
+    'spaceone.identity.api.v1.authorization': ['Authorization']
 }
