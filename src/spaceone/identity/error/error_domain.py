5ae828d937195b178073b4fa9c40041182ea1d4d
Jongmin Kim <whdalsrnt@megazone.com>
2020-12-09 15:11:23 +0900

    Add error classes
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/spaceone/identity/error/error_domain.py
+++ src/spaceone/identity/error/error_domain.py
@@ -1,8 +1,8 @@
 from spaceone.core.error import *
 
 
-class ERROR_USER_STATUS_CHECK_FAILURE(ERROR_BASE):
-    _message = 'A user "{user_id}" status is not ENABLED.'
+class ERROR_DOMAIN_STATE(ERROR_BASE):
+    _message = 'Domain is disabled (domain_id = {domain_id})'
 
 
 class ERROR_EXTERNAL_USER_NOT_ALLOWED_API_USER(ERROR_INVALID_ARGUMENT):
