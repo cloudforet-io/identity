ebd6df808b6213b0e77efb7def0c8ead02b2be93
Jongmin Kim <whdalsrnt@megazone.com>
2021-01-21 21:03:15 +0900

    Insert reference_id into model using reference field.
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>
    (cherry picked from commit 0119d1ea7687ef147ef9f26e52ee12c03d42bb6f)

--- src/spaceone/identity/info/role_binding_info.py
+++ src/spaceone/identity/info/role_binding_info.py
@@ -15,18 +15,28 @@ def RoleBindingInfo(role_binding_vo: RoleBinding, minimal=False):
         'role_binding_id': role_binding_vo.role_binding_id,
         'resource_type': role_binding_vo.resource_type,
         'resource_id': role_binding_vo.resource_id,
-        'role_info': RoleInfo(role_binding_vo.role, minimal=True) if role_binding_vo.role else None,
-        'project_info': ProjectInfo(role_binding_vo.project, minimal=True) if role_binding_vo.project else None,
-        'project_group_info': ProjectGroupInfo(role_binding_vo.project_group, minimal=True) if role_binding_vo.project_group else None
+        'role_info': RoleInfo(role_binding_vo.role, minimal=True) if role_binding_vo.role else None
     }
 
     if not minimal:
         info.update({
+            'project_info': ProjectInfo(role_binding_vo.project, minimal=True) if role_binding_vo.project else None,
+            'project_group_info': ProjectGroupInfo(role_binding_vo.project_group, minimal=True) if role_binding_vo.project_group else None,
             'labels': role_binding_vo.labels,
             'tags': [tag_pb2.Tag(key=tag.key, value=tag.value) for tag in role_binding_vo.tags],
             'domain_id': role_binding_vo.domain_id,
             'created_at': change_timestamp_type(role_binding_vo.created_at)
         })
+
+        if not role_binding_vo.project_id and role_binding_vo.project:
+            role_binding_vo.update({'project_id': role_binding_vo.project.project_id})
+
+        if not role_binding_vo.project_group_id and role_binding_vo.project_group:
+            role_binding_vo.update({'project_group_id': role_binding_vo.project_group.project_group_id})
+
+    # Temporary code for DB migration
+    if not role_binding_vo.role_id and role_binding_vo.role:
+        role_binding_vo.update({'role_id': role_binding_vo.role.role_id})
 
     return role_binding_pb2.RoleBindingInfo(**info)
 
