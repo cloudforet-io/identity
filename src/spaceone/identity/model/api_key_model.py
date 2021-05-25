0e33f38f9e0e10f295f23d3607c60c54a08b6cd3
Jongmin Kim <whdalsrnt@megazone.com>
2021-05-25 17:10:23 +0900

    API key cascade deletion applied when deleting a user
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/spaceone/identity/model/api_key_model.py
+++ src/spaceone/identity/model/api_key_model.py
@@ -1,11 +1,13 @@
 from mongoengine import *
 from spaceone.core.model.mongo_model import MongoModel
+from spaceone.identity.model.user_model import User
 
 
 class APIKey(MongoModel):
     api_key_id = StringField(max_length=40, generate_id='api-key', unique=True)
     state = StringField(max_length=20, default='ENABLED', choices=('ENABLED', 'DISABLED'))
     user_id = StringField(max_length=40, required=True)
+    user = ReferenceField('User', null=True, default=None, reverse_delete_rule=CASCADE)
     domain_id = StringField(max_length=40, required=True)
     last_accessed_at = DateTimeField(default=None, null=True)
     created_at = DateTimeField(auto_now_add=True)
