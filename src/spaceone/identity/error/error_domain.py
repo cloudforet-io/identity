d24fa91c2b5eaf74771fd19eb2e6cb1ed969709f
“Choonho <choonhoson@megazone.com>
2021-06-08 00:22:02 +0000

    update auth API
    Signed-off-by: “Choonho <choonhoson@megazone.com>

--- src/spaceone/identity/error/error_domain.py
+++ src/spaceone/identity/error/error_domain.py
@@ -4,6 +4,8 @@ from spaceone.core.error import *
 class ERROR_DOMAIN_STATE(ERROR_BASE):
     _message = 'Domain is disabled (domain_id = {domain_id})'
 
+class ERROR_DOMAIN_AUTH_PLUGIN(ERROR_BASE):
+    _message = 'Failed to get endpoint of version = {version}, domain_id = {domain_id}'
 
 class ERROR_EXTERNAL_USER_NOT_ALLOWED_API_USER(ERROR_INVALID_ARGUMENT):
     _message = 'External user cannot be created with the API_USER type.'
