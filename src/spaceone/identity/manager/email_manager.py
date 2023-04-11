76754dca78c8c5203e91a9da2a0e70974dcf358f
ImMin5 <mino@megazone.com>
2023-04-11 15:03:21 +0900

    feat: modify LANGUAGE_MAPPER at email_manager (#31)

--- src/spaceone/identity/manager/email_manager.py
+++ src/spaceone/identity/manager/email_manager.py
@@ -15,9 +15,27 @@ JINJA_ENV = Environment(
 )
 
 LANGUAGE_MAPPER = {
-    'ko': 'ko',
-    'en': 'en',
-    'jp': 'jp'
+    'default': {
+        'reset_password': 'Reset your password',
+        'temp_password': 'Your password has been changed',
+        'verify_email': 'Verify your notification email',
+    },
+    'ko': {
+        'reset_password': '비밀번호 재설정 안내',
+        'temp_password': '임시 비밀번호 발급 안내',
+        'verify_email': '알림전용 이메일 계정 인증 안내',
+    },
+    'en': {
+        'reset_password': 'Reset your password',
+        'temp_password': 'Your password has been changed',
+        'verify_email': 'Verify your notification email',
+    },
+    'jp': {
+        'reset_password': 'パスワードリセットのご案内',
+        'temp_password': '仮パスワード発行のご案内',
+        'verify_email': '通知メールアカウント認証のご案内',
+    }
+
 }
 
 
@@ -28,82 +46,52 @@ class EmailManager(BaseManager):
         self.smtp_connector: SMTPConnector = self.locator.get_connector('SMTPConnector')
 
     def send_reset_password_email(self, user_id, email, reset_password_link, language):
-        language = LANGUAGE_MAPPER.get(language, 'en')
         service_name = self._get_service_name()
+        language_map_info = LANGUAGE_MAPPER.get(language, 'default')
 
         template = JINJA_ENV.get_template(f'reset_pwd_link_when_pw_forgotten_{language}.html')
         email_contents = template.render(user_name=user_id, reset_password_link=reset_password_link, service_name=service_name)
-
-        if language == 'ko':
-            subject = f'[{service_name}] 비밀번호 재설정 안내'
-        elif language == 'jp':
-            subject = f'[{service_name}] パスワードリセットのご案内'
-        else:
-            subject = f'[{service_name}] Reset your password'
+        subject = f'[{service_name}] {language_map_info["reset_password"]}'
 
         self.smtp_connector.send_email(email, subject, email_contents)
 
     def send_temporary_password_email(self, user_id, email, console_link, temp_password, language):
-        language = LANGUAGE_MAPPER.get(language, 'en')
         service_name = self._get_service_name()
+        language_map_info = LANGUAGE_MAPPER.get(language, 'default')
 
         template = JINJA_ENV.get_template(f'temp_pwd_when_pw_forgotten_{language}.html')
         email_contents = template.render(user_name=user_id, temp_password=temp_password, service_name=service_name, login_link=console_link)
-
-        if language == 'ko':
-            subject = f'[{service_name}] 임시 비밀번호 발급 안내'
-        elif language == 'jp':
-            subject = f'[{service_name}] 仮パスワード発行のご案内'
-        else:
-            subject = f'[{service_name}] Your password has been changed'
+        subject = f'[{service_name}] {language_map_info["temp_password"]}'
 
         self.smtp_connector.send_email(email, subject, email_contents)
 
     def send_reset_password_email_when_user_added(self, user_id, email, reset_password_link, language):
-        language = LANGUAGE_MAPPER.get(language, 'en')
         service_name = self._get_service_name()
+        language_map_info = LANGUAGE_MAPPER.get(language, 'default')
 
         template = JINJA_ENV.get_template(f'reset_pwd_link_when_user_added_{language}.html')
         email_contents = template.render(user_name=user_id, reset_password_link=reset_password_link, service_name=service_name)
-
-        if language == 'ko':
-            subject = f'[{service_name}] 비밀번호 재설정 안내'
-        elif language == 'jp':
-            subject = f'[{service_name}] パスワードリセットのご案内'
-        else:
-            subject = f'[{service_name}] Reset your password'
+        subject = f'[{service_name}] {language_map_info["reset_password"]}'
 
         self.smtp_connector.send_email(email, subject, email_contents)
 
     def send_temporary_password_email_when_user_added(self, user_id, email, console_link, temp_password, language):
-        language = LANGUAGE_MAPPER.get(language, 'en')
         service_name = self._get_service_name()
+        language_map_info = LANGUAGE_MAPPER.get(language, 'default')
 
         template = JINJA_ENV.get_template(f'temp_pwd_when_user_added_{language}.html')
         email_contents = template.render(user_name=user_id, temp_password=temp_password, service_name=service_name, login_link=console_link)
-
-        if language == 'ko':
-            subject = f'[{service_name}] 비밀번호 재설정 안내'
-        elif language == 'jp':
-            subject = f'[{service_name}] パスワードリセットのご案内'
-        else:
-            subject = f'[{service_name}] Reset your password'
+        subject = f'[{service_name}] {language_map_info["temp_password"]}'
 
         self.smtp_connector.send_email(email, subject, email_contents)
 
     def send_verification_email(self, user_id, email, verification_code, language):
-        language = LANGUAGE_MAPPER.get(language, 'en')
         service_name = self._get_service_name()
+        language_map_info = LANGUAGE_MAPPER.get(language, 'default')
 
         template = JINJA_ENV.get_template(f'verification_code_{language}.html')
         email_contents = template.render(user_name=user_id, verification_code=verification_code, service_name=service_name)
-
-        if language == 'ko':
-            subject = f'[{service_name}] 알림전용 이메일 계정 인증 안내'
-        elif language == 'jp':
-            subject = f'[{service_name}] 通知メールアカウント認証のご案内'
-        else:
-            subject = f'[{service_name}] Verify your notification email'
+        subject = f'[{service_name}] {language_map_info["verify_email"]}'
 
         self.smtp_connector.send_email(email, subject, email_contents)
 
