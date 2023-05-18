095ef27e7a5432894c5ce39650c442ae21113a1b
ImMin5 <mino@megazone.com>
2023-05-18 15:06:33 +0900

    feat: modify token timeout (#50)

--- src/spaceone/identity/conf/global_conf.py
+++ src/spaceone/identity/conf/global_conf.py
@@ -28,6 +28,7 @@ CACHES = {
 IDENTITY = {
     'token': {
         'verify_code_timeout': 3600,
+        'temporary_token_timeout': 86400,  # 24 hours
         'token_timeout': 1200,
         'refresh_timeout': 1800,
         'refresh_ttl': 18,
