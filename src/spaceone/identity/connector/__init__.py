c831c9577b08a9cd46c28c937e27b26011f9d98c
Jongmin Kim <whdalsrnt@megazone.com>
2023-11-23 09:21:23 +0900

    refactor: remove unused imports

--- src/spaceone/identity/connector/__init__.py
+++
@@ -1,2 +0,0 @@
-from spaceone.identity.connector.smtp_connector import SMTPConnector
-from spaceone.identity.connector.auth_plugin_connector import AuthPluginConnector
--- src/spaceone/identity/manager/__init__.py
+++
@@ -1,7 +0,0 @@
-from spaceone.identity.manager.mfa_manager.email_mfa_manger import EmailMFAManager
-from spaceone.identity.manager.token_manager.local_token_manager import (
-    LocalTokenManager,
-)
-from spaceone.identity.manager.token_manager.external_token_manager import (
-    ExternalTokenManager,
-)
