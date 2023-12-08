280a3ee0f180049353541865669f495f78a024a2
Jongmin Kim <whdalsrnt@megazone.com>
2023-12-08 23:16:35 +0900

    feat: apply for identity v2

--- src/spaceone/identity/service/domain_service.py
+++ src/spaceone/identity/service/domain_service.py
@@ -3,6 +3,7 @@ from typing import Union
 
 from spaceone.core.service import *
 from spaceone.core.service.utils import *
+from spaceone.core import utils
 
 from spaceone.identity.manager.external_auth_manager import ExternalAuthManager
 from spaceone.identity.manager.domain_manager import DomainManager
@@ -200,7 +201,7 @@ class DomainService(BaseService):
         """
 
         pub_jwk = self.domain_secret_mgr.get_domain_public_key(params.domain_id)
-        return DomainSecretResponse(public_key=str(pub_jwk), domain_id=params.domain_id)
+        return DomainSecretResponse(public_key=utils.dump_json(pub_jwk), domain_id=params.domain_id)
 
     @transaction(scope='system_admin:read')
     @append_query_filter(["domain_id", "name", "state"])
