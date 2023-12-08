b12bed8074935b40961837df05f8187dc5b8c7e6
ImMin5 <mino@megazone.com>
2023-12-09 00:05:30 +0900

    feat: temporary exclude all handler (#73)
    Signed-off-by: ImMin5 <mino@megazone.com>

--- src/spaceone/identity/service/domain_service.py
+++ src/spaceone/identity/service/domain_service.py
@@ -18,10 +18,10 @@ from spaceone.identity.error.error_domain import *
 _LOGGER = logging.getLogger(__name__)
 
 
-@authentication_handler
-# @authorization_handler
-@mutation_handler
-@event_handler
+# @authentication_handler
+# # @authorization_handler
+# @mutation_handler
+# @event_handler
 class DomainService(BaseService):
     service = "identity"
     resource = "Domain"
