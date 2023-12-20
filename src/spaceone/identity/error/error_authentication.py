83c9a2fd30ffadece4cc544a5c79e86b6ad061ba
Jongmin Kim <whdalsrnt@megazone.com>
2023-12-20 17:35:05 +0900

    refactor: remove unused error class

--- src/spaceone/identity/error/error_authentication.py
+++ src/spaceone/identity/error/error_authentication.py
@@ -9,10 +9,6 @@ class ERROR_NOT_AUTHENTICATED(ERROR_AUTHENTICATE_FAILURE):
     _message = "A user is not authenticated."
 
 
-class ERROR_NOT_ALLOWED_ISSUE_TOKEN_API_USER(ERROR_AUTHENTICATE_FAILURE):
-    _message = "API_USER are not allowed to issue tokens. (user_id = {user_id})"
-
-
 class ERROR_INVALID_CREDENTIALS(ERROR_AUTHENTICATE_FAILURE):
     _message = "Invalid credentials provided."
 
@@ -23,15 +19,3 @@ class ERROR_AUTHENTICATION_FAILURE_PLUGIN(ERROR_INTERNAL_API):
 
 class ERROR_INVALID_GRANT_TYPE(ERROR_INVALID_ARGUMENT):
     _message = "Invalid grant type. (grant_type = {grant_type})"
-
-
-class ERROR_REFRESH_COUNT(ERROR_AUTHENTICATE_FAILURE):
-    _message = "The refresh count is exhausted."
-
-
-class ERROR_NOT_FOUND_PRIVATE_KEY(ERROR_AUTHENTICATE_FAILURE):
-    _message = "Private key not found. (purpose = {purpose})"
-
-
-class ERROR_MAX_REFRESH_COUNT(ERROR_AUTHENTICATE_FAILURE):
-    _message = "The maximum refresh count has been exceeded. (max_refresh_count = {max_refresh_count})"
