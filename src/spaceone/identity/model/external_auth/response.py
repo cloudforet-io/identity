c5a5c052ae41a1d6ada91e5a0c0bc86ada839745
ImMin5 <mino@megazone.com>
2023-12-29 11:44:10 +0900

    fix: convert data type 'datetime' -> 'string' at ExternalAuthResponse
    Signed-off-by: ImMin5 <mino@megazone.com>

--- src/spaceone/identity/model/external_auth/response.py
+++ src/spaceone/identity/model/external_auth/response.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from typing import Union, Literal
+
 from pydantic import BaseModel, Field
+from spaceone.core import utils
 
 __all__ = ["ExternalAuthResponse"]
 
@@ -22,3 +24,8 @@ class ExternalAuthResponse(BaseModel):
     state: Union[State, None] = None
     plugin_info: Union[Plugin, None] = None
     updated_at: Union[datetime, None] = None
+
+    def dict(self, *args, **kwargs):
+        data = super().dict(*args, **kwargs)
+        data["created_at"] = utils.datetime_to_iso8601(data["created_at"])
+        return data
