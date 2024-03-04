cdd769bc28c56056eae3714b4845cd961d789bf7
ImMin5 <mino@megazone.com>
2024-03-04 21:18:02 +0900

    feat: add plugin interface and skeleton code (#199)
    Signed-off-by: ImMin5 <mino@megazone.com>

---
+++ src/spaceone/identity/plugin/account_collector/model/account_collect_response.py
@@ -0,0 +1,21 @@
+from typing import Union, List
+from pydantic import BaseModel, Field
+
+__all__ = ["PluginResponse", "AccountsResponse"]
+
+
+class PluginResponse(BaseModel):
+    metadata: dict
+
+
+class AccountResponse(BaseModel):
+    name: Union[str, None] = None
+    data: Union[dict, None] = None
+    secret_schema_id: Union[str, None] = None
+    secret_data: Union[dict, None] = None
+    tags: Union[dict, None] = None
+    location: Union[list, None] = None
+
+
+class AccountsResponse(BaseModel):
+    results: List[AccountResponse] = None
