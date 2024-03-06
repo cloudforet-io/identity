01a228e4ac5606f91d49636f30173bfe3a5a013a
ImMin5 <mino@megazone.com>
2024-03-06 12:29:46 +0900

    feat: modify identity plugin interface (#199)
    Signed-off-by: ImMin5 <mino@megazone.com>

--- src/spaceone/identity/plugin/account_collector/interface/grpc/account_collector.py
+++ src/spaceone/identity/plugin/account_collector/interface/grpc/account_collector.py
@@ -4,22 +4,22 @@ from spaceone.api.identity.plugin import (
     account_collector_pb2_grpc,
 )
 from spaceone.identity.plugin.account_collector.service.account_collector_service import (
-    AccountCollectService,
+    AccountCollectorService,
 )
 
 
-class AccountCollector(BaseAPI, account_collector_pb2_grpc):
+class AccountCollector(BaseAPI, account_collector_pb2_grpc.AccountCollectorServicer):
     pb2 = account_collector_pb2
     pb2_grpc = account_collector_pb2_grpc
 
     def init(self, request, context):
         params, metadata = self.parse_request(request, context)
-        account_collector_svc = AccountCollectService(metadata)
+        account_collector_svc = AccountCollectorService(metadata)
         response: dict = account_collector_svc.init(params)
         return self.dict_to_message(response)
 
     def sync(self, request, context):
         params, metadata = self.parse_request(request, context)
-        account_collector_svc = AccountCollectService(metadata)
+        account_collector_svc = AccountCollectorService(metadata)
         response: dict = account_collector_svc.sync(params)
         return self.dict_to_message(response)
