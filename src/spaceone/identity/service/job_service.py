cb88b7dc12a395b3fee238d563b3dfbb9b91b6d2
ImMin5 <mino@megazone.com>
2024-08-08 08:25:11 +0900

    fix: fix list secrets method at secret manager
    Signed-off-by: ImMin5 <mino@megazone.com>

--- src/spaceone/identity/service/job_service.py
+++ src/spaceone/identity/service/job_service.py
@@ -1,7 +1,7 @@
 import logging
 import random
 from datetime import datetime, timedelta
-from typing import Union, List, Tuple
+from typing import Union, List
 
 from spaceone.core.service import *
 from spaceone.core.service.utils import *
@@ -69,7 +69,7 @@ class JobService(BaseService):
         current_hour = params.get("current_hour", datetime.utcnow().hour)
 
         for trusted_account_vo in self._get_all_schedule_enabled_trusted_accounts(
-                current_hour
+            current_hour
         ):
             try:
                 self.create_service_account_job(trusted_account_vo, {})
@@ -303,9 +303,9 @@ class JobService(BaseService):
 
     @staticmethod
     def _get_this_month_cost(
-            cost_analysis_mgr: CostAnalysisManager,
-            domain_id: str,
-            workspace_id: str,
+        cost_analysis_mgr: CostAnalysisManager,
+        domain_id: str,
+        workspace_id: str,
     ) -> float:
         system_token = config.get_global("TOKEN")
         now = datetime.utcnow()
@@ -508,7 +508,7 @@ class JobService(BaseService):
         )
 
     def create_service_account_job(
-            self, trusted_account_vo: TrustedAccount, job_options: dict
+        self, trusted_account_vo: TrustedAccount, job_options: dict
     ) -> Union[Job, dict]:
         resource_group = trusted_account_vo.resource_group
         provider = trusted_account_vo.provider
@@ -611,10 +611,10 @@ class JobService(BaseService):
         return secret_data
 
     def _check_duplicate_job(
-            self,
-            domain_id: str,
-            trusted_account_id: str,
-            this_job_vo: Job,
+        self,
+        domain_id: str,
+        trusted_account_id: str,
+        this_job_vo: Job,
     ) -> bool:
         query = {
             "filter": [
@@ -638,7 +638,7 @@ class JobService(BaseService):
         return False
 
     def _is_job_failed(
-            self, job_id: str, domain_id: str, workspace_id: str = None
+        self, job_id: str, domain_id: str, workspace_id: str = None
     ) -> bool:
         job_vo: Job = self.job_mgr.get_job(domain_id, job_id, workspace_id)
 
@@ -648,10 +648,10 @@ class JobService(BaseService):
             return False
 
     def _close_job(
-            self,
-            job_id: str,
-            domain_id: str,
-            workspace_id: str = None,
+        self,
+        job_id: str,
+        domain_id: str,
+        workspace_id: str = None,
     ):
         job_vo: Job = self.job_mgr.get_job(domain_id, job_id, workspace_id)
         if job_vo.status == "IN_PROGRESS":
@@ -660,7 +660,7 @@ class JobService(BaseService):
             self.job_mgr.update_job_by_vo({"finished_at": datetime.utcnow()}, job_vo)
 
     def _create_workspace(
-            self, domain_id: str, trusted_account_id: str, location_info: dict
+        self, domain_id: str, trusted_account_id: str, location_info: dict
     ) -> Workspace:
         name = location_info.get("name")
         reference_id = location_info.get("resource_id")
@@ -706,12 +706,12 @@ class JobService(BaseService):
         return workspace_vo
 
     def _create_project_group(
-            self,
-            domain_id: str,
-            workspace_id: str,
-            trusted_account_id: str,
-            location_info: dict,
-            parent_group_id: str = None,
+        self,
+        domain_id: str,
+        workspace_id: str,
+        trusted_account_id: str,
+        location_info: dict,
+        parent_group_id: str = None,
     ) -> ProjectGroup:
         name = location_info["name"]
         reference_id = location_info["resource_id"]
@@ -761,14 +761,14 @@ class JobService(BaseService):
         return project_group_vo
 
     def _create_project(
-            self,
-            result: dict,
-            domain_id: str,
-            workspace_id: str,
-            trusted_account_id: str,
-            project_group_id: str = None,
-            sync_options: dict = None,
-            project_type: str = "PRIVATE",
+        self,
+        result: dict,
+        domain_id: str,
+        workspace_id: str,
+        trusted_account_id: str,
+        project_group_id: str = None,
+        sync_options: dict = None,
+        project_type: str = "PRIVATE",
     ) -> Project:
         name = result["name"]
         reference_id = result["resource_id"]
@@ -807,13 +807,13 @@ class JobService(BaseService):
         return project_vo
 
     def _create_service_account(
-            self,
-            result: dict,
-            project_vo: Project,
-            trusted_account_id: str,
-            trusted_secret_id: str,
-            provider: str,
-            sync_options: dict = None,
+        self,
+        result: dict,
+        project_vo: Project,
+        trusted_account_id: str,
+        trusted_secret_id: str,
+        provider: str,
+        sync_options: dict = None,
     ) -> Union[ServiceAccount, None]:
         domain_id = project_vo.domain_id
         workspace_id = project_vo.workspace_id
@@ -872,7 +872,7 @@ class JobService(BaseService):
             secret_mgr: SecretManager = self.locator.get_manager("SecretManager")
             secret_id = service_account_vo.secret_id
 
-            response = secret_mgr.list_secrets({"secret_id": secret_id})
+            response = secret_mgr.list_secrets({"secret_id": secret_id}, domain_id)
             secret_total_count = response.get("total_count", 0)
 
             if secret_total_count > 0:
@@ -916,7 +916,7 @@ class JobService(BaseService):
         return service_account_vo
 
     def _remove_old_reference_id_from_workspace(
-            self, domain_id: str, workspace_id: str, reference_id: str
+        self, domain_id: str, workspace_id: str, reference_id: str
     ) -> None:
         query = {
             "filter": [
