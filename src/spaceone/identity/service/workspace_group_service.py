953a443ff830c05bd1fc9a3edf9a2d7d7658802e
Youngjin Jo <youngjinjo@megazone.com>
2024-08-28 15:30:49 +0900

    chore: remove comments
    Signed-off-by: Youngjin Jo <youngjinjo@megazone.com>

--- src/spaceone/identity/service/workspace_group_service.py
+++ src/spaceone/identity/service/workspace_group_service.py
@@ -2,34 +2,54 @@ import logging
 from datetime import datetime
 from typing import Union
 
-from spaceone.core.error import (ERROR_EXIST_RESOURCE, ERROR_INVALID_PARAMETER,
-                                 ERROR_NOT_FOUND, ERROR_PERMISSION_DENIED)
-from spaceone.core.service import (BaseService, authentication_handler,
-                                   authorization_handler, event_handler,
-                                   mutation_handler, transaction)
-from spaceone.core.service.utils import (append_keyword_filter,
-                                         append_query_filter, convert_model)
+from spaceone.core.error import (
+    ERROR_EXIST_RESOURCE,
+    ERROR_INVALID_PARAMETER,
+    ERROR_NOT_FOUND,
+    ERROR_PERMISSION_DENIED,
+)
+from spaceone.core.service import (
+    BaseService,
+    authentication_handler,
+    authorization_handler,
+    event_handler,
+    mutation_handler,
+    transaction,
+)
+from spaceone.core.service.utils import (
+    append_keyword_filter,
+    append_query_filter,
+    convert_model,
+)
 
 from spaceone.identity.error import ERROR_ROLE_DOES_NOT_EXIST_OF_USER
-from spaceone.identity.error.error_role import (ERROR_NOT_ALLOWED_ROLE_TYPE,
-                                                ERROR_NOT_ALLOWED_USER_STATE)
+from spaceone.identity.error.error_role import (
+    ERROR_NOT_ALLOWED_ROLE_TYPE,
+    ERROR_NOT_ALLOWED_USER_STATE,
+)
 from spaceone.identity.manager.role_binding_manager import RoleBindingManager
 from spaceone.identity.manager.role_manager import RoleManager
 from spaceone.identity.manager.user_manager import UserManager
-from spaceone.identity.manager.workspace_group_manager import \
-    WorkspaceGroupManager
+from spaceone.identity.manager.workspace_group_manager import WorkspaceGroupManager
 from spaceone.identity.manager.workspace_manager import WorkspaceManager
-from spaceone.identity.manager.workspace_user_manager import \
-    WorkspaceUserManager
+from spaceone.identity.manager.workspace_user_manager import WorkspaceUserManager
 from spaceone.identity.model.workspace_group.request import (
-    WorkspaceGroupAddUsersRequest, WorkspaceGroupAddWorkspacesRequest,
-    WorkspaceGroupCreateRequest, WorkspaceGroupDeleteRequest,
-    WorkspaceGroupGetRequest, WorkspaceGroupRemoveUsersRequest,
-    WorkspaceGroupRemoveWorkspacesRequest, WorkspaceGroupSearchQueryRequest,
-    WorkspaceGroupStatQueryRequest, WorkspaceGroupUpdateRequest,
-    WorkspaceGroupUpdateRoleRequest)
+    WorkspaceGroupAddUsersRequest,
+    WorkspaceGroupAddWorkspacesRequest,
+    WorkspaceGroupCreateRequest,
+    WorkspaceGroupDeleteRequest,
+    WorkspaceGroupGetRequest,
+    WorkspaceGroupRemoveUsersRequest,
+    WorkspaceGroupRemoveWorkspacesRequest,
+    WorkspaceGroupSearchQueryRequest,
+    WorkspaceGroupStatQueryRequest,
+    WorkspaceGroupUpdateRequest,
+    WorkspaceGroupUpdateRoleRequest,
+)
 from spaceone.identity.model.workspace_group.response import (
-    WorkspaceGroupResponse, WorkspaceGroupsResponse)
+    WorkspaceGroupResponse,
+    WorkspaceGroupsResponse,
+)
 from spaceone.identity.service.role_binding_service import RoleBindingService
 
 _LOGGER = logging.getLogger(__name__)
@@ -280,12 +300,6 @@ class WorkspaceGroupService(BaseService):
         workspace_group_vo = self.workspace_group_mgr.get_workspace_group(
             params.workspace_group_id, params.domain_id
         )
-
-        # if not workspace_group_vo.workspaces:
-        #     raise ERROR_WORKSPACES_DO_NOT_EXIST(
-        #         key="workspaces",
-        #         reason=f"Workspace Group {params.workspace_group_id} does not have any workspaces.",
-        #     )
 
         new_users = list(set([user_info["user_id"] for user_info in params.users]))
         old_users = []
