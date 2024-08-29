fd9f6f03d8a64736b54b93d48f2c7fef7e646e39
Youngjin Jo <youngjinjo@megazone.com>
2024-08-29 16:06:56 +0900

    feat: add list_basic_role method
    Signed-off-by: Youngjin Jo <youngjinjo@megazone.com>

--- src/spaceone/identity/model/role/response.py
+++ src/spaceone/identity/model/role/response.py
@@ -1,11 +1,12 @@
 from datetime import datetime
-from typing import Union, List
+from typing import List, Union
+
 from pydantic import BaseModel
 from spaceone.core import utils
 
 from spaceone.identity.model.role.request import RoleType, State
 
-__all__ = ["RoleResponse", "RolesResponse"]
+__all__ = ["RoleResponse", "RolesResponse", "BasicRoleResponse", "BasicRolesResponse"]
 
 
 class RoleResponse(BaseModel):
@@ -30,4 +31,25 @@ class RoleResponse(BaseModel):
 
 class RolesResponse(BaseModel):
     results: List[RoleResponse]
+    total_count: int
+
+
+class BasicRoleResponse(BaseModel):
+    role_id: Union[str, None] = None
+    name: Union[str, None] = None
+    state: Union[State, None] = None
+    role_type: Union[RoleType, None] = None
+    domain_id: Union[str, None] = None
+    created_at: Union[datetime, None] = None
+    updated_at: Union[datetime, None] = None
+
+    def dict(self, *args, **kwargs):
+        data = super().dict(*args, **kwargs)
+        data["created_at"] = utils.datetime_to_iso8601(data["created_at"])
+        data["updated_at"] = utils.datetime_to_iso8601(data["updated_at"])
+        return data
+
+
+class BasicRolesResponse(BaseModel):
+    results: List[BasicRoleResponse]
     total_count: int
