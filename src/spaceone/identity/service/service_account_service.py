a4003e26ead5881b6f10e4cc6f0121a93c3a1b6e
mz-ko <hyupko@megazone.com>
2024-12-04 10:36:15 +0900

    remove: delete un-use annotation line.
    Signed-off-by: MZC01-HYUPKO <mzc01-hyupko@MZC01-HYUPKO.local>

--- src/spaceone/identity/service/service_account_service.py
+++ src/spaceone/identity/service/service_account_service.py
@@ -468,7 +468,6 @@ class ServiceAccountService(BaseService):
             "user_projects",
         ]
     )
-    @append_keyword_filter(["service_account_id", "name"])
     @set_query_page_limit(2000)
     @convert_model
     def analyze(self, params: ServiceAccountAnalyzeQueryRequest) -> dict:
