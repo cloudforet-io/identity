10ca53f80b1c8e6e8b0844c01bb8407b7ad3fa1d
lhhyung <lhhyung511@megazone.com>
2025-01-03 14:48:02 +0900

    feat: Add internal_endpoint to endpoint list
    Signed-off-by: lhhyung <lhhyung511@megazone.com>

--- src/spaceone/identity/model/endpoint/request.py
+++ src/spaceone/identity/model/endpoint/request.py
@@ -1,10 +1,12 @@
-from typing import Union
+from typing import Union, Literal
 from pydantic import BaseModel
 
 __all__ = ["EndpointSearchQueryRequest"]
+
+EndpointType = Literal["PUBLIC", "INTERNAL"]
 
 
 class EndpointSearchQueryRequest(BaseModel):
     query: Union[dict, None] = None
     service: Union[str, None] = None
-    endpoint_type: Union[str, None] = None
+    endpoint_type: Union[EndpointType, None] = None
